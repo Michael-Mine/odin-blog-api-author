bbb9d7d021d3261e502e16992ea1ff3e0dcdb5cb
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-05-01 20:45:58 +0100

    feat: add useAllPosts to Post page & amend

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,21 +1,20 @@
-import { useOutletContext, useParams } from "react-router";
+import { useParams } from "react-router";
+import useAllPosts from "../hooks/useAllPosts";
 import formatDate from "../utils/formatDate";
-import Login from "../components/Login";
-import WriteComment from "../components/WriteComment";
 import Comments from "../components/Comments";
 import styles from "../styles/Post.module.css";
-import { useState } from "react";
 
 function Post() {
-  const [allPosts] = useOutletContext();
-  const [loggedIn, setLoggedIn] = useState(false);
+  const { allPosts, error, loading } = useAllPosts();
+
   let { postId } = useParams();
   const post = allPosts.find((post) => post.id == postId);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>A network error was encountered</p>;
   if (!post) return <h2>Post Not Found</h2>;
 
   const date = formatDate(post.datePublished);
-  const token = localStorage.getItem("JWT");
 
   return (
     <div>
@@ -27,16 +26,15 @@ function Post() {
       <div className={styles.post}>
         <h2>{post.title}</h2>
         <p>
-          By Mr Mine - Published on {date.toDateString()} at{" "}
-          {date.toLocaleTimeString()}
+          {post.datePublished
+            ? "Published on " +
+              date.toDateString() +
+              " at " +
+              date.toLocaleTimeString()
+            : "Not Published"}
         </p>
         <p>{post.content}</p>
       </div>
-      {token || loggedIn ? (
-        <WriteComment setLoggedIn={setLoggedIn} />
-      ) : (
-        <Login setLoggedIn={setLoggedIn} />
-      )}
       <Comments />
     </div>
   );
